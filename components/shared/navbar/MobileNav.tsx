10adbf4cef2ce5da2c01a7020630f3981b9fb8b6
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-09-29 20:56:53 +0200

    add leftsidebar

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -18,18 +18,25 @@ const NavContent = () => {
   return (
     <section className="flex h-full flex-col gap-6 pt-16">
       {sidebarLinks.map((item) => {
-        const isActive = pathname.includes(item.route) && item.route.length > 1 || pathname === item.route;
+        const isActive =
+          (pathname.includes(item.route) && item.route.length > 1) ||
+          pathname === item.route;
         return (
           <SheetClose asChild key={item.label}>
-            <Link href={item.route} className={`${isActive? 'primary-gradient rounded-xl text-light-900': 'text-dark300_light900'} flex items-center justify-start gap-4 bg-transparent p-4`}>
+            <Link
+              href={item.route}
+              className={`${isActive ? "primary-gradient rounded-xl text-light-900" : "text-dark300_light900"} flex items-center justify-start gap-4 bg-transparent p-4`}
+            >
               <Image
                 height={20}
                 width={20}
                 src={item.imgURL}
                 alt={item.label}
-                className={`${isActive? '': 'invert-colors'}`}
+                className={`${isActive ? "" : "invert-colors"}`}
               />
-              <p  className={`${isActive? 'base-bold': 'base-medium'}`}>{item.label}</p>
+              <p className={`${isActive ? "base-bold" : "base-medium"}`}>
+                {item.label}
+              </p>
             </Link>
           </SheetClose>
         );
@@ -66,28 +73,28 @@ const MobileNav = () => {
           </p>
         </Link>
         <div>
-        <SheetClose asChild>
-          <NavContent />
-        </SheetClose>
+          <SheetClose asChild>
+            <NavContent />
+          </SheetClose>
 
-        <SignedOut>
-          <div className="flex flex-col gap-3">
-            <SheetClose asChild>
-              <Link href="/sign-in">
-                <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-xl px-4 py-3 shadow-none">
-                  <span className="primary-text-gradient">Log In</span>
-                </Button> 
-              </Link>
-            </SheetClose>
-            <SheetClose asChild>
-              <Link href="/sign-up">
-                <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-xl px-4 py-3 shadow-none">
-                  Sign Up
-                </Button>
-              </Link>
-            </SheetClose>
-          </div>
-        </SignedOut>
+          <SignedOut>
+            <div className="flex flex-col gap-3">
+              <SheetClose asChild>
+                <Link href="/sign-in">
+                  <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-xl px-4 py-3 shadow-none">
+                    <span className="primary-text-gradient">Log In</span>
+                  </Button>
+                </Link>
+              </SheetClose>
+              <SheetClose asChild>
+                <Link href="/sign-up">
+                  <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-xl px-4 py-3 shadow-none">
+                    Sign Up
+                  </Button>
+                </Link>
+              </SheetClose>
+            </div>
+          </SignedOut>
         </div>
       </SheetContent>
     </Sheet>
