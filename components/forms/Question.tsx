d2166672be092d253d32b6ce6d06080ec3c60d1b
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-11-10 00:45:52 +0100

    add tinymce editor

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,5 +1,6 @@
 "use client"
-
+import { useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
@@ -19,6 +20,7 @@ import React from 'react'
 import { QuestionsSchema } from "@/lib/validations"
 
 const Question = () => {
+    const editorRef = useRef(null);
 
     const form = useForm<z.infer<typeof QuestionsSchema>>({
         resolver: zodResolver(QuestionsSchema),
@@ -61,7 +63,27 @@ const Question = () => {
           <FormItem className="flex w-full flex-col gap-3">
             <FormLabel className="paragraph-semibold text-dark400_light800">Question Explanation <span className=" text-primary-500">*</span></FormLabel>
             <FormControl  className="mt-3.5 border-light-700">
-              //editor
+              <Editor
+        apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+        onInit={(_evt, editor) =>{ 
+            //@ts-ignore
+            editorRef.current = editor
+        }}
+        initialValue=""
+        init={{
+          height: 350,
+          menubar: false,
+          plugins: [
+            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+            'anchor', 'searchreplace', 'visualblocks', 'codesample', 'fullscreen',
+            'insertdatetime', 'media', 'table'
+          ],
+          toolbar: 'undo redo | blocks |' +
+            'codesample  bold italic forecolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' ,
+          content_style: 'body { font-family:Inter; font-size:16px }'
+        }}
+      />
             </FormControl>
             <FormDescription className="body-regular mt-2.5 text-light-500">
               Introduse the problem and expand on what you put in the Title. Minimum 100 characters.
