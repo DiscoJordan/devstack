3b22f505c00dce7e98bd17ec65f04a6881eba3c1
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-11-09 01:55:32 +0100

    add Filters component

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,8 @@
+import HomeFilters from "@/components/home/HomeFilters";
+import Filter from "@/components/shared/Filter";
 import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
 import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 import React from "react";
 
@@ -16,11 +19,23 @@ const Home = () => {
       </div>
       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
         <LocalSearchBar
-        route='/' iconPosition='left' imgSrc= '/assets/icons/search.svg' placeholder='Search for questions' otherClasses= 'flex-1'
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
         ></LocalSearchBar>
-        Filters
 
+        <Filter 
+        filters={HomePageFilters}
+        otherClasses='min-h-[56px] sm:min-w-[170px]'
+        containerClasses= 'hidden max-md:flex'
+        ></Filter>
       </div>
+
+      <HomeFilters >
+        
+      </HomeFilters>
     </>
   );
 };
---
+++ components/shared/Filter.tsx
@@ -0,0 +1,50 @@
+"use client";
+import React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+interface Props {
+  filters: {
+    name: string;
+    value: string;
+  }[];
+  otherClasses?: string;
+  containerClasses?: string;
+}
+
+const Filter = ({ filters, otherClasses, containerClasses }: Props) => {
+  return (
+    <div className={`relative ${containerClasses}`}>
+      <Select>
+        <SelectTrigger
+          className={`${otherClasses} body-regular light-border background-light800_dark300 text-dark500_light700 rounded-[8px] border-0 px-5 py-2.5`}
+        >
+          <div className="line-clamp-1 flex-1 text-left">
+            <SelectValue placeholder="Select a filter" />
+          </div>
+        </SelectTrigger>
+        <SelectContent className="background-light800_dark300 text-dark500_light700 ">
+            <SelectGroup>
+                {filters.map((filter) => (
+            <SelectItem key={filter.value} value={filter.value}>
+              {filter.name}
+            </SelectItem>
+          ))}
+            </SelectGroup>
+          
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
+
+export default Filter;
